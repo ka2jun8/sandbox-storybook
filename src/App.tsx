cd8fff29b189c4b7f4c3bddbc481a8be96c06929
ka2jun8 <ka210n8@gmail.com>
2021-01-27 23:43:48 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import "./styles.css";
 
 type Props = {
   title: string;
-  description: string;
+  descriptions: string[];
 };
 
 export default function App(props: Props) {
   return (
     <div className="App">
       <h1>{props.title}</h1>
-      <h2>{props.description}</h2>
+      <h2>{props.descriptions.join("/")}</h2>
     </div>
   );
 }
